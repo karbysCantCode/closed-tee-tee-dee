0bb8d7fddcf03f4e6cb3a73fcb918434883f7c10
karbysCantCode <realgeekscorp@gmail.com>
2024-09-09 08:14:25 +1200

    Add project files.

---
+++ SDL template.cpp
@@ -0,0 +1,106 @@
+    // SDL template.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "SDL.h"
+#include "TickRateMadeEasy.h"
+#include "isometricProjection.h"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!\n";
+
+    int screenX = 240;
+    int screenY = 400;
+
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    {
+        if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+            std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+            return 1; // Exit with error code
+        }
+
+        window = SDL_CreateWindow(
+            "My SDL Window",               // Window title
+            SDL_WINDOWPOS_CENTERED,        // X position, centered
+            SDL_WINDOWPOS_CENTERED,        // Y position, centered
+            screenX,                           // Width
+            screenY,                           // Height
+            SDL_WINDOW_SHOWN                // Window flags
+        );
+
+        if (window == nullptr) {
+            std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+            SDL_Quit(); // Clean up SDL
+            return 1; // Exit with error code
+        }
+
+        renderer = SDL_CreateRenderer(
+            window,              // Window to associate with
+            -1,                  // Use the first rendering driver
+            SDL_RENDERER_ACCELERATED // Renderer flags
+        );
+
+        if (renderer == nullptr) {
+            std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+            SDL_DestroyWindow(window);
+            SDL_Quit(); // Clean up SDL
+            return 1; // Exit with error code
+        }
+    }
+
+    vertMap map(120, 200, 10);
+    map.generateHeights();
+   // map.printRawData();
+    SDL_Texture* texture = map.generateTexture(renderer);
+    std::cout << "done!\n";
+
+    bool running = true;
+
+    SDL_Event event;
+
+    const int tickRate = 60;
+
+    const auto tickInterval = TRME_getTickInterval(tickRate);
+
+    while (running) {
+        auto startTime = TRME_getTimePoint();
+
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+            else if (event.type == SDL_MOUSEBUTTONDOWN) {
+                int mouseX, mouseY;
+                SDL_GetMouseState(&mouseX, &mouseY);
+                
+            }
+            // Add more event handling as needed (e.g., keyboard input, mouse clicks)
+        }
+
+        SDL_RenderClear(renderer);
+        
+        // rendering stuff here
+        {
+            SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+        }
+        SDL_RenderPresent(renderer);
+        TRME_sleepUntilNextTick(startTime, tickInterval);
+    }
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+// 
+// Drop SDL2-2.30.3 in the same directory as the SLN
+// 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ isometricProjection.h
@@ -0,0 +1,251 @@
+#pragma once
+#include "vector"
+#include <array>
+#include <random>
+
+#include <map>
+
+
+
+std::array<char, 31> asciiCharacters = { '.', ',', ':', ';', '-', '_', '=', '*', '+', '~', '/', '|', '\\', '\'', '\"', '^', '(', ')', '{', '}', '[', ']', '<', '>', '!', '?', '%', '@', '$', '&', '#' };
+
+enum tileSkin {
+	GRASS,
+	SAND,
+	WATER
+};
+struct vertice {
+	short y;
+	tileSkin tileType;
+};
+
+template <typename T>
+T randomNumber(const T min, const T max) {
+	static std::random_device rd;   // Seed for random number generator
+	static std::mt19937 gen(rd());  // Mersenne Twister engine
+
+	if constexpr (std::is_integral<T>::value) { // it is an int or variant of int
+		std::uniform_int_distribution<T> dis(min, max);
+		return dis(gen);
+	}
+	else if constexpr (std::is_floating_point<T>::value) { //is it a float or varaint of flaot
+		std::uniform_real_distribution<T> dis(min, max);
+		return dis(gen);
+	}
+}
+
+short randomShort(const short min, const short max) {
+	static std::random_device rd;   // Seed for random number generator
+	static std::mt19937 gen(rd());  // Mersenne Twister engine
+
+	std::uniform_int_distribution<short> dis(min, max);
+	return dis(gen);
+}
+
+class vertMap {
+public:
+	int width, height;
+	int smoothness; // 0 - 100
+	const int capHeight = 255;
+
+	const int rngtop = 1000;
+
+	const int down = 1 * rngtop/100;
+	const int stay = 5 * rngtop/100;
+	const int up   = 100 * rngtop/100;
+	
+	// h * height + w
+	// y * height + x
+	// where x is wrapped under y
+	std::vector<vertice> allVertices;
+
+	std::map<int, int> frequencyCount;
+
+	vertMap(const int w, const int h, const int smooth) : width(w), height(h), smoothness(smooth) {
+		allVertices.resize(height * height + width);
+	}
+
+	short solveLowestVertice(const int x, const int y) {
+		short lowest = capHeight;
+		//lowest = ((((y - 1) * height + (x - 1)) >= 0) * (((y - 1) * height + (x - 1)) < allVertices.size())) * (allVertices[(y - 1) * height + (x - 1)].y)
+
+		{
+			const int v = (y - 1) * height + (x - 1);
+			if (v >= 0 && v < allVertices.size()) {
+				const int val = allVertices[v].y;
+				if (val < lowest) {
+					lowest = val;
+				}
+			}
+		}
+		{
+			const int v = (y - 1) * height + (x);
+			if (v >= 0 && v < allVertices.size()) {
+				const int val = allVertices[v].y;
+				if (val < lowest) {
+					lowest = val;
+				}
+			}
+		}
+		{
+			const int v = (y - 1) * height + (x + 1);
+			if (v >= 0 && v < allVertices.size()) {
+				const int val = allVertices[v].y;
+				if (val < lowest) {
+					lowest = val;
+				}
+			}
+		}
+		{
+			const int v = (y) * height + (x - 1);
+			if (v >= 0 && v < allVertices.size()) {
+				const int val = allVertices[v].y;
+				if (val < lowest) {
+					lowest = val;
+				}
+			}
+		}
+		{
+			const int v = (y) * height + (x + 1);
+			if (v >= 0 && v < allVertices.size()) {
+				const int val = allVertices[v].y;
+				if (val < lowest) {
+					lowest = val;
+				}
+			}
+		}
+		{
+			const int v = (y + 1) * height + (x - 1);
+			if (v >= 0 && v < allVertices.size()) {
+				const int val = allVertices[v].y;
+				if (val < lowest) {
+					lowest = val;
+				}
+			}
+		}
+		{
+			const int v = (y + 1) * height + (x);
+			if (v >= 0 && v < allVertices.size()) {
+				const int val = allVertices[v].y;
+				if (val < lowest) {
+					lowest = val;
+				}
+			}
+		}
+		{
+			const int v = (y + 1) * height + (x + 1);
+			if (v >= 0 && v < allVertices.size()) {
+				const int val = allVertices[v].y;
+				if (val < lowest) {
+					lowest = val;
+				}
+			}
+		}
+		return lowest;
+	}
+
+	short solveVertHeight(const int x, const int y) {
+		const short lowest = solveLowestVertice(x, y);
+
+		const short random = randomShort(0, rngtop);
+		const short b = ((random <= down) * -1) + ((random > stay) * 1);
+		const short nRes = (b + lowest);
+
+		// Ensure nRes is within the valid range
+		short res = nRes;
+		if (res < 0) {
+			res = 0;
+		}
+		else if (res > capHeight) {
+			res = capHeight;
+		}
+
+		frequencyCount[lowest] += 1;
+
+		// Debug output
+		std::cout << "x: " << x << ", y: " << y << ", random: " << random << ", b: " << b << ", nRes: " << nRes << ", res: " << res << ", lowest" << lowest << std::endl;
+
+		return res;
+	}
+
+	void generateHeights() {
+		for (size_t y = 0; y < height; y++)
+		{
+			for (size_t x = 0; x < width; x++)
+			{
+				allVertices[y * height + x].y = solveVertHeight(x, y);
+				//std::cout << y * height + x << '\n';
+			}
+		}
+
+		for (const auto& the : frequencyCount) {
+			std::cout << the.first << ':' << the.second << '\n';
+		}
+		std::cout << down << ':' << stay << ':' << up << '\n';
+	}
+	
+	void printRawData() {
+		for (size_t y = 0; y < height; y++)
+		{
+			for (size_t x = 0; x < width; x++)
+			{
+				std::cout << asciiCharacters[allVertices[y * height + x].y] << " ";
+			}
+			std::cout << "\n";
+		}
+	}
+
+	SDL_Texture* generateTexture(SDL_Renderer* renderer) {
+		SDL_Surface* surface = SDL_CreateRGBSurface(0, width, height, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
+		if (!surface) {
+			std::cerr << "Failed to create surface! SDL_Error: " << SDL_GetError() << std::endl;
+			return nullptr;
+		}
+
+		if (SDL_MUSTLOCK(surface)) {
+			SDL_LockSurface(surface);
+		}
+		Uint32* pixels = static_cast<Uint32*>(surface->pixels); // Assuming 32-bit pixels
+		int pitch = surface->pitch / sizeof(Uint32); // Number of Uint32 pixels per row
+
+		for (int y = 0; y < surface->h; ++y) {
+			for (int x = 0; x < surface->w; ++x) {
+				Uint32* pixel = pixels + y * pitch + x;
+
+				// Modify the pixel value
+				const Uint8 v = 255 / (capHeight / 10) * allVertices[y * height + x].y;
+				Uint8 r = v;
+				Uint8 g = v;
+				Uint8 b = v;
+				Uint8 a = 255; // Opaque
+
+				// Set the new pixel value
+				*pixel = SDL_MapRGBA(surface->format, r, g, b, a);
+			}
+		}
+
+		if (SDL_MUSTLOCK(surface)) {
+			SDL_UnlockSurface(surface);
+		}
+
+		SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+		SDL_FreeSurface(surface); // Free the surface after creating the texture
+
+		if (!texture) {
+			std::cerr << "Failed to create texture from surface! SDL_Error: " << SDL_GetError() << std::endl;
+		}
+
+		return texture;
+	}
+
+};
+class viewport {
+public:
+
+	int screenX, screenY;
+	SDL_Renderer* renderer;
+
+	viewport(const int scrnX, const int scrnY, SDL_Renderer* render) : screenX(scrnX), screenY(scrnY), renderer(render) {}
+
+
+};
